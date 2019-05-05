f790c4aea4f216fc28ce306c652e17bf00731a47
ZiJie.Yip <438623841@qq.com>
2019-05-05 17:28:30 +0800

    项目资源添加

--- src/main/java/com/yip/config/WebMvcConfig.java
+++ src/main/java/com/yip/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -25,6 +26,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter implements Application
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     /**
